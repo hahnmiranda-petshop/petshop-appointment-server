475e24e37a3ef2908e5e869a6336a76a2eb1de24
hahnmiranda <hahnmiranda@gmail.com>
2025-08-09 15:20:18 -0300

    docs: adicionando esqueleto do projeto com arquivos base.

---
+++ src/main/java/com/hahnmiranda/petshop/petshopappointmentserver/PetshopAppointmentServerApplication.java
@@ -0,0 +1,13 @@
+package com.hahnmiranda.petshop.petshopappointmentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopAppointmentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopAppointmentServerApplication.class, args);
+    }
+
+}
